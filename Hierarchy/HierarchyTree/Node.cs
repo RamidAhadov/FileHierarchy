b05802f8b7029d0c19e499014eb3184990074477
RamidAhadov <ramidehedov02@gmail.com>
2024-03-11 00:00:01 +0400

    Parent change tracking

--- Hierarchy/HierarchyTree/Node.cs
+++ Hierarchy/HierarchyTree/Node.cs
@@ -2,15 +2,30 @@ namespace Hierarchy.HierarchyTree;
 
 public abstract class Node
 {
+    private FolderNode _parent;
     public Node(string name, FolderNode? parent, NodeType type)
     {
         Name = name;
         Path = parent == null ? "../" : Utilities.Path.SetPath(parent.Path, parent.Name);
-        Parent = parent;
+        _parent = parent;
         Type = type;
     }
     public string Name { get; set; }
     public string Path { get; set; }
-    public FolderNode? Parent { get; set; }
+
+    public FolderNode? Parent
+    {
+        get => _parent;
+        set
+        {
+            _parent = value;
+            UpdatePath(_parent);
+        }
+    }
     public NodeType Type { get; set; }
+
+    private void UpdatePath(FolderNode parent)
+    {
+        Path = parent == null ? "../" : Utilities.Path.SetPath(parent.Path, parent.Name);
+    }
 }
